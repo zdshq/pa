93f350b78e1ccd1110a0a99ab857cb5700da788a
tracer-ics2022 <tracer@njuics.org>
2023-07-08 21:08:59 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:08:59 up  2:58,  1 user,  load average: 0.70, 1.14, 1.18

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -13,7 +13,7 @@
 # See the Mulan PSL v2 for more details.
 #**************************************************************************************/
 
--include $(NEMU_HOME)/../Makefile
+include $(NEMU_HOME)/../Makefile
 include $(NEMU_HOME)/scripts/build.mk
 
 include $(NEMU_HOME)/tools/difftest.mk
