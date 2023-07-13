55cf9532a2fe07a36a6be16de65eb24b328e1577
tracer-ics2022 <tracer@njuics.org>
2023-07-13 18:48:19 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:48:19 up  9:20,  1 user,  load average: 1.23, 1.20, 1.14

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -24,7 +24,7 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_S, TYPE_J,
+  TYPE_I, TYPE_U, TYPE_S, TYPE_J, TYPE_B, TYPE_R,
   TYPE_N, // none
 };
 
@@ -35,6 +35,14 @@ enum {
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 #define immJ() do { *imm = (SEXT(BITS(i, 19, 12), 8) << 12) | (SEXT(BITS(i, 20, 20),1) << 11) \
       | (SEXT(BITS(i, 30, 21),10) << 1) | (SEXT(BITS(i, 31, 30),10) << 20);} while(0)
+#define immB() do { *imm = (SEXT(BITS(i, 7, 7), 1) << 11) | (SEXT(BITS(i, 11, 8), 4) << 1) \
+      | (SEXT(BITS(i, 30, 25), 6) << 5 | (SEXT(BITS(i, 31, 31), 1) << 12)); } while(0)
+
+#define handle_bne          \
+{                           \
+  if(src1 != src2)          \
+    s->dnpc = PC + imm;     \
+}
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
@@ -46,6 +54,8 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_J:                   immJ(); break;
+    case TYPE_B: src1R(); src2R(); immB(); break;
+    case TYPE_R: src1R(); src2R();         break;
   }
 }
 
@@ -68,7 +78,10 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 010 ????? 00000 11", lw     , I, R(rd) = Mr(src1 + imm, 4));
   INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw     , S, Mw(src1 + imm, 4, src2));
   INSTPAT("??????? ????? ????? ??? ????? 11001 11", jalr   , S, {R(rd) = PC + 4; s->dnpc = (src1 + imm) & ~1;});
-
+  INSTPAT("??????? ????? ????? 001 ????? 11000 11", bne    , B, handle_bne); // bne
+  INSTPAT("0000001 ????? ????? 100 ????? 11000 11", div    , R, R(rd) = src1/src2); // bne
+  INSTPAT("0100000 ????? ????? 000 ????? 01100 11", sub    , R, R(rd) = src1-src2); // bne
+  INSTPAT("0000000 ????? ????? 100 ????? 01100 11", xor    , R, R(rd) = src1^src2); // bne
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
