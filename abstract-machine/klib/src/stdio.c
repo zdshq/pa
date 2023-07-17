7d89c711ac4dd3cb89b2face314538cb8fcffe65
tracer-ics2022 <tracer@njuics.org>
2023-07-17 19:29:12 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:29:12 up  7:52,  1 user,  load average: 1.52, 1.02, 0.85

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,8 +13,97 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+enum{
+    INT_TYPE = 0,
+    STR_TYPE,
+    TYPE_END
+};
+
+typedef struct t_escape_type
+{
+    int type;
+    char str[4];
+}escape_type;
+
+
+escape_type find_escape(const char *fmt, int *seek){
+    escape_type type_table[TYPE_END + 1] = {
+        {INT_TYPE, "%d"},
+        {STR_TYPE, "%s"},
+        {TYPE_END, ""},
+    };
+    int i;
+    for(i = *seek; fmt[i] != '\0'; i++){
+        if(fmt[i] == '%'){
+            for(int j = 0; j != TYPE_END; j++){
+                int len = strlen(type_table[j].str);
+                if(strncmp(fmt + i, type_table[j].str, len) == 0)
+                {
+                    *seek = i + strlen(type_table[j].str);
+                    return type_table[j];
+                }
+                    
+            }
+        }
+    }
+    *seek = i;
+    return type_table[TYPE_END];
+}
+
+int myitoa(int a, char* out){
+    int len = 0, int_temp;
+    int_temp = a;
+    do{
+        len++;
+        int_temp /= 10;
+    }while(int_temp);
+    for(int i = 0; i < len; i++){
+        out[i] = a % 10 + '0';
+        a /= 10;
+    }
+    return len;
+}
+
+int sprintf(char *out, const char *fmt,...)
+{
+    va_list ap;
+    escape_type temp;
+    int index = 0;
+    int count = 0;;
+    int last_i = 0;
+    int int_temp;
+    char *str_temp;
+    va_start(ap, fmt);
+    for (int i = 0; fmt[i] != '\0'; ++i) {
+        temp = find_escape(fmt, &i);
+        if (temp.type == TYPE_END)
+        {
+            continue;
+        }
+        switch (temp.type)
+        {
+        case INT_TYPE:
+            int_temp = va_arg(ap, int);
+            strncpy(out + index, fmt + last_i, i - (strlen(temp.str)) - last_i);
+            index += i - last_i -strlen(temp.str);
+            index += myitoa(int_temp, out+index);
+            break;
+        case STR_TYPE:
+            str_temp = va_arg(ap, char *);
+            strncpy(out + index, fmt + last_i, i - (strlen(temp.str)) - last_i);
+            index += i - last_i -strlen(temp.str);
+            strncpy(out+index, str_temp, strlen(str_temp));   
+            index += strlen(str_temp);             
+            break;    
+        default:
+            break;
+        }
+        count++;
+        last_i = i;
+    }
+    va_end(ap);
+    out[index] = '\0';
+    return count;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
