07f126848cd51a92fbcee20cb1db07f3accdfc5d
tracer-ics2022 <tracer@njuics.org>
2023-07-18 17:12:57 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:12:57 up 17:33,  1 user,  load average: 1.27, 1.03, 1.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -116,7 +116,7 @@ static void execute(uint64_t n) {
     trace_and_difftest(&s, cpu.pc);
     if (nemu_state.state != NEMU_RUNNING) 
     {
-      if (nemu_state.state == NEMU_ABORT)
+      if (nemu_state.halt_ret != 0)
         show(&s);
       break;
     }
