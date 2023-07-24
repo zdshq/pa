e18825721bc55d3c30b82bff0c320007989261fa
tracer-ics2022 <tracer@njuics.org>
2023-07-24 23:46:27 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:46:27 up  8:01,  1 user,  load average: 0.74, 0.74, 0.55

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -63,12 +63,22 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_FTRACE_COND
   // func_log_write(_this->logbuf);
   // if(find_str)
-  if(find_str(_this->logbuf, "ret")){
+  if (find_str(_this->logbuf, "jal") || find_str(_this->logbuf, "jalr") ){
+    for(int i = 0; i < func_index; i++){
+      if(_this->dnpc >= func_info[i].start && _this->dnpc < (func_info[i].start + func_info[i].size)){
+        char str[100];
+        sprintf(str,"pc:%lu\t%ld:call func:%s\n", _this->pc, func_index, func_info[i].func_name);
+        printf("%s", str);
+      }
+    }    
+  }
+  else if(find_str(_this->logbuf, "ret")){
     uint64_t temp = StrToInt(_this->logbuf, 18);
     for(int i = 0; i < func_index; i++){
       if(temp >= func_info[i].start && temp < (func_info[i].start + func_info[i].size)){
         char str[100];
-        sprintf(str,"%ld:ret func:%s", func_index ,func_info[i].func_name);
+        sprintf(str,"pc:%lu\t%ld:ret func:%s\n", _this->pc, func_index,func_info[i].func_name);
+        printf("%s", str);
       }
     }
   }
