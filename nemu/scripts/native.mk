d60b4b295c4ef1d1db7556eaf3f7efa5d0732921
tracer-ics2022 <tracer@njuics.org>
2023-07-25 23:11:05 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:11:05 up 12:46,  1 user,  load average: 0.67, 0.98, 0.75

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -24,7 +24,7 @@ $(BINARY): compile_git
 
 # Some convenient rules
 
-override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
+# override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
 # override ARGS += --mem=$(BUILD_DIR)/nemu-mem.txt
 override ARGS += $(ARGS_DIFF)
 
