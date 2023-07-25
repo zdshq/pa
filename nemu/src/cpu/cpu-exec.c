6473a6c6bffe0ef2b17103d74ee724bcca1a5878
tracer-ics2022 <tracer@njuics.org>
2023-07-25 21:33:30 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:33:30 up 11:09,  1 user,  load average: 0.80, 0.81, 0.76

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -63,27 +63,27 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 // #endif
 #ifdef CONFIG_FTRACE_COND
   // func_log_write(_this->logbuf);
-  // if(find_str)
-  if (find_str(_this->logbuf, "jal") || find_str(_this->logbuf, "jalr") ){
-    if(_this->isa.inst.val == 0x00008067){
-      printf("myret\n");
-      for(int i = 0; i < func_index; i++){
-        if(_this->pc >= func_info[i].start && _this->pc < (func_info[i].start + func_info[i].size)){
-          char str[100];
-          sprintf(str,"pc:%lx\t%ld:ret func:%s\n", _this->pc, --call_index, func_info[i].func_name);
-          printf("%s", str);
-        }
-      }
-    }
-    else{
-      for(int i = 0; i < func_index; i++){
-        if(_this->dnpc >= func_info[i].start && _this->dnpc < (func_info[i].start + func_info[i].size)){
-          char str[100];
-          sprintf(str,"pc:%lx\t%ld:call func:%s\n", _this->pc, call_index++, func_info[i].func_name);
-          printf("%s", str);
-        }
-      }         
-    }
+  // // if(find_str)
+  // if (find_str(_this->logbuf, "jal") || find_str(_this->logbuf, "jalr") ){
+  //   if(_this->isa.inst.val == 0x00008067){
+  //     printf("myret\n");
+  //     for(int i = 0; i < func_index; i++){
+  //       if(_this->pc >= func_info[i].start && _this->pc < (func_info[i].start + func_info[i].size)){
+  //         char str[100];
+  //         sprintf(str,"pc:%lx\t%ld:ret func:%s\n", _this->pc, --call_index, func_info[i].func_name);
+  //         printf("%s", str);
+  //       }
+  //     }
+  //   }
+  //   else{
+  //     for(int i = 0; i < func_index; i++){
+  //       if(_this->dnpc >= func_info[i].start && _this->dnpc < (func_info[i].start + func_info[i].size)){
+  //         char str[100];
+  //         sprintf(str,"pc:%lx\t%ld:call func:%s\n", _this->pc, call_index++, func_info[i].func_name);
+  //         printf("%s", str);
+  //       }
+  //     }         
+  //   }
  
   }
 
