a1dae5324c24874abd5e8e7cbe60929786ab3ca3
tracer-ics2022 <tracer@njuics.org>
2023-07-26 21:09:40 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:09:40 up  9:30,  1 user,  load average: 0.96, 1.20, 1.22

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -23,12 +23,12 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint32_t *p = ctl->pixels;
-  uint64_t a;
+  // uint64_t a;
   if(ctl->w != 0 && ctl->h != 0){
     for(uint32_t i = ctl->x ; i < (ctl->x+ctl->w) && i < (inl(VGACTL_ADDR) >> 16); i++){
       for(uint32_t j = ctl->y; j < (ctl->y+ctl->h) && j < (uint16_t)(inl(VGACTL_ADDR)); j++){
-        a = (uint64_t)(FB_ADDR + (j * (uint16_t)(inl(VGACTL_ADDR) >> 16) + i)*sizeof(uint32_t));
-        printf("i:%d, j:%d h:%d\r\n", i, j, a);
+        // a = (uint64_t)(FB_ADDR + (j * (uint16_t)(inl(VGACTL_ADDR) >> 16) + i)*sizeof(uint32_t));
+        // printf("i:%d, j:%d h:%d\r\n", i, j, a);
         outl(FB_ADDR + ((j * (uint16_t)(inl(VGACTL_ADDR) >> 16) + i)*sizeof(uint32_t)), p[j*ctl->w+i]);
       }
     }    
