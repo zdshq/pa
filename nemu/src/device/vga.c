8ff1b330895de328406e268c5d4b67f736135ee3
tracer-ics2022 <tracer@njuics.org>
2023-07-26 21:10:08 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:10:08 up  9:31,  1 user,  load average: 0.77, 1.13, 1.19

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -87,7 +87,7 @@ void vga_update_screen() {
 void init_vga() {
   vgactl_port_base = (uint32_t *)new_space(8);
   vgactl_port_base[0] = (screen_width() << 16) | screen_height();
-  printf("h:%d\t w:%d\r\n",screen_width(),screen_height());
+  printf("h:%d\t w:%d\r\n",screen_height(),screen_width());
 #ifdef CONFIG_HAS_PORT_IO
   add_pio_map ("vgactl", CONFIG_VGA_CTL_PORT, vgactl_port_base, 8, NULL);
 #else
