845fee8efed3bccd632be37dbd3ecd74753a48bc
tracer-ics2022 <tracer@njuics.org>
2023-07-26 15:03:14 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:03:14 up  3:24,  1 user,  load average: 3.36, 1.59, 1.13

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -6,8 +6,8 @@
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   // kbd->keydown = 0;
   // kbd->keycode = AM_KEY_NONE;
-  if (inl(KBD_ADDR)) {
-    int code = inl(KBD_ADDR);
+  int code = inl(KBD_ADDR);
+  if (code) {
     kbd->keydown = code & KEYDOWN_MASK;
     kbd->keycode = code & ~(KEYDOWN_MASK);
   } else {
