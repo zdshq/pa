f99eb6d182c0d61bb5b52da50978609c38d9bbf5
tracer-ics2022 <tracer@njuics.org>
2023-07-26 21:31:13 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:31:13 up  9:52,  1 user,  load average: 0.79, 1.06, 1.62

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -22,48 +22,19 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  // uint32_t* fb = (uint32_t*)(uintptr_t)FB_ADDR;
-  // uint32_t* pixels = (uint32_t*)(ctl->pixels);
-  // int x = ctl->x, y = ctl->y;
-  // int w = ctl->w, h = ctl->h;
-  // for (int j = 0; j < h; j++) {
-  //   for (int i = 0; i < w; i++) {
-  //     fb[(y + j) * 400 + (x + i)] = *(pixels + j * w + i);
-  //   }
-  // }
-
-  // if (ctl->sync) {
-  //   outl(SYNC_ADDR, 1);
-  // }
-  uint32_t *p = ctl->pixels;
-  // uint64_t a;
-  if(ctl->w != 0 && ctl->h != 0){
-    // printf()
-    for(uint32_t i = ctl->x ; i < (ctl->x+ctl->w) && i < (inl(VGACTL_ADDR) >> 16); i++){
-      for(uint32_t j = ctl->y; j < (ctl->y+ctl->h) && j < (uint16_t)(inl(VGACTL_ADDR)); j++){
-        // a = (uint64_t)(FB_ADDR + (j * (uint16_t)(inl(VGACTL_ADDR) >> 16) + i)*sizeof(uint32_t));
-        // printf("i:%d, j:%d h:%d\r\n", i, j, a);
-        outl(FB_ADDR + ((j * 400) + i)*sizeof(uint32_t), p[j*ctl->w+i]);
-      }
-    }    
-    // printf("break\r\n");
-  }
-  else{
-    if (ctl->sync) {      
-      printf("hhhaaaaaaaaaaaaaaa\r\n");
-      outl(SYNC_ADDR, 1);
-    }    
+  uint32_t* fb = (uint32_t*)(uintptr_t)FB_ADDR;
+  uint32_t* pixels = (uint32_t*)(ctl->pixels);
+  int x = ctl->x, y = ctl->y;
+  int w = ctl->w, h = ctl->h;
+  for (int j = 0; j < h; j++) {
+    for (int i = 0; i < w; i++) {
+      fb[(y + j) * 400 + (x + i)] = *(pixels + j * w + i);
+    }
   }
 
-
-  // int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-  // if (w == 0 || h == 0) return;
-  // feclearexcept(-1);
-  // SDL_Surface *s = SDL_CreateRGBSurfaceFrom(ctl->pixels, w, h, 32, w * sizeof(uint32_t),
-  //     RMASK, GMASK, BMASK, AMASK);
-  // SDL_Rect rect = { .x = x, .y = y };
-  // SDL_BlitSurface(s, NULL, surface, &rect);
-  // SDL_FreeSurface(s);
+  if (ctl->sync) {
+    outl(SYNC_ADDR, 1);
+  }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
