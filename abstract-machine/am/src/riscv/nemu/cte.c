e30491d2f3062690d51862da46ce044f7586ca57
tracer-ics2022 <tracer@njuics.org>
2023-07-27 19:09:17 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:09:17 up 10:05,  1 user,  load average: 1.11, 1.19, 1.01

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -6,6 +6,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
+    printf("%d",1);
     Event ev = {0};
     switch (c->mcause) {
       default: ev.event = EVENT_ERROR; break;
@@ -23,7 +24,7 @@ extern void __am_asm_trap(void);
 bool cte_init(Context*(*handler)(Event, Context*)) {
   // initialize exception entry
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
-  asm volatile("csrw mstatus, %0" : : "r"(0xa00001800));
+  // asm volatile("csrw mstatus, %0" : : "r"(0xa00001800));
   // register event handler
   user_handler = handler;
 
