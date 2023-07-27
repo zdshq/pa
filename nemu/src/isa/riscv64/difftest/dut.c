79ffa3e3adb6a4d4de360db51b90fa77d7512066
tracer-ics2022 <tracer@njuics.org>
2023-07-27 15:31:04 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:31:04 up  6:27,  1 user,  load average: 0.92, 0.83, 0.71

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -18,19 +18,31 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  if ((memcmp(ref_r->gpr, cpu.gpr, sizeof(word_t) * 33) == 0))
-    return true;
-  // printf("---------------------------dut------------------------------\n");
-//   for (size_t i = 0; i < 16; i++) {
-//     printf("%s:%16p\t\t%s:%16p\n", reg_name(i, 64), (void*)cpu.gpr[i], reg_name(i + 16, 64), (void*)cpu.gpr[i + 16]);
-//   }
-//   printf("\tpc:%16p\n", (void*)cpu.pc);
-//   printf("---------------------------ref------------------------------\n");
-//   for (size_t i = 0; i < 16; i++) {
-//     printf("%s:%16p\t\t%s:%16p\n", reg_name(i, 64), (void*)ref_r->gpr[i], reg_name(i + 16, 64), (void*)ref_r->gpr[i + 16]);
-//   }
-//   printf("\tpc:%16p\n", (void*)ref_r->pc);
-  return false;
+  bool ret = true;
+  for (int i = 0; i < 32; i++) {
+    if (ref_r->gpr[i] != cpu.gpr[i]) {
+      ret = false;
+    }
+  }
+  if (ref_r->pc != cpu.pc) {
+    printf("pc,err!!");
+    ret = false;
+  }
+
+  if (ret != true) {
+    printf("---------------------------dut------------------------------\n");
+    for (size_t i = 0; i < 16; i++) {
+      printf("%s:%16p\t\t%s:%16p\n", reg_name(i, 64), (void*)cpu.gpr[i], reg_name(i + 16, 64), (void*)cpu.gpr[i + 16]);
+    }
+    printf("\tpc:%16p\n", (void*)cpu.pc);
+    printf("---------------------------ref------------------------------\n");
+    for (size_t i = 0; i < 16; i++) {
+      printf("%s:%16p\t\t%s:%16p\n", reg_name(i, 64), (void*)ref_r->gpr[i], reg_name(i + 16, 64), (void*)ref_r->gpr[i + 16]);
+    }
+    printf("\tpc:%16p\n", (void*)ref_r->pc);
+  }
+
+  return ret;
 }
 
 void isa_difftest_attach() {
