bdb182a6529de8d34700076f95d5b9a2d21b3b14
tracer-ics2022 <tracer@njuics.org>
2023-07-27 15:36:05 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:36:05 up  6:32,  1 user,  load average: 0.75, 0.81, 0.74

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -19,7 +19,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-  printf("\nisa_raise_intr ok \n");
+  printf("\nisa_raise_intr ok  NO:%ld \n", NO);
   cpu.csr[mepc] = epc;
   cpu.csr[mcause] = NO;
   cpu.csr[mstatus] |= 0x1800;
