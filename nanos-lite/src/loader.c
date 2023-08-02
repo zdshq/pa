296230929e5d7e2cdb0734b68f8c9016633a583d
tracer-ics2022 <tracer@njuics.org>
2023-08-02 21:58:38 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:58:38 up  2:32,  1 user,  load average: 0.88, 0.87, 0.72

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -11,10 +11,18 @@
 
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
+void show_ehdr(Elf64_Ehdr e){
+  printf("--------------------------ehdr info-------------------------\n");
+  printf("e_phoff : %ld\n",e.e_phoff);
+  printf("e_phnum : %d\n",e.e_phnum);
+  printf("--------------------------ehdr end--------------------------\n");
+}
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
   printf("1111\n");
   Elf64_Ehdr ehdr;
   ramdisk_read(&ehdr, 0, sizeof(Elf64_Ehdr));
+  show_ehdr(ehdr);
   printf("\n%s\n",ehdr.e_ident);
   return 0;
 }
