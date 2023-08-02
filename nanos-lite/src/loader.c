0ca2d04b7dac6740c7ba8b8a89395e3d98fa5c1b
tracer-ics2022 <tracer@njuics.org>
2023-08-02 21:39:38 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:39:38 up  2:13,  1 user,  load average: 0.51, 0.66, 0.66

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,6 +1,6 @@
 #include <proc.h>
 #include <elf.h>
-
+#include <stdio.h>
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
 # define Elf_Phdr Elf64_Phdr
@@ -9,14 +9,19 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
+  printf("1111\n");
+  Elf64_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(Elf64_Ehdr));
+  printf("\n%s\n",ehdr.e_ident);
   return 0;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
-  Log("Jump to entry = %p", entry);
+  // Log("Jump to entry = %p", entry);
   ((void(*)())entry) ();
 }
 
