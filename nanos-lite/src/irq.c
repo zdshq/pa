5f9a9b96284e1149a16cf13bcc16feeefebb69e3
tracer-ics2022 <tracer@njuics.org>
2023-08-03 18:24:36 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:24:36 up  3:47,  1 user,  load average: 1.48, 1.08, 0.73

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,7 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_NULL :  printf("exit\n"); break;
+    case EVENT_NULL :  halt(0); break;
     case EVENT_YIELD:  printf("EVENT_YIELD\n"); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
