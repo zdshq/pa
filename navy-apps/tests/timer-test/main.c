47359e89c4ea9ede8025339e3fa3636ded9bb2bc
tracer-ics2022 <tracer@njuics.org>
2023-08-05 00:13:09 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:13:09 up 11:37,  1 user,  load average: 1.41, 1.55, 1.17

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -3,34 +3,34 @@
 #include <sys/time.h>
 #include "NDL.h"
 int main() {
-  struct timeval tv;
-  __uint64_t last, now;
-  int i;
-  gettimeofday(&tv, NULL);
-  now = tv.tv_sec * 1000 + tv.tv_usec / 1000;
-  last = now;
-  // printf("hhhh\n");
-  while (1) {
-    gettimeofday(&tv, NULL);
-    now = tv.tv_sec * 1000 + tv.tv_usec / 1000;
-    // printf("hhhh\n");
-    if (now - last >= 1000) {
-      last = now;
-      printf("timer-test:%d\n", i++);
-    }
-  }
-
-  // __uint32_t last, now;
-  // now = NDL_GetTicks();
-  // last = now;
+  // struct timeval tv;
+  // __uint64_t last, now;
   // int i;
+  // gettimeofday(&tv, NULL);
+  // now = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+  // last = now;
+  // // printf("hhhh\n");
   // while (1) {
-  //   now = NDL_GetTicks();
+  //   gettimeofday(&tv, NULL);
+  //   now = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+  //   // printf("hhhh\n");
   //   if (now - last >= 1000) {
   //     last = now;
   //     printf("timer-test:%d\n", i++);
   //   }
   // }
+
+  __uint32_t last, now;
+  now = NDL_GetTicks();
+  last = now;
+  int i;
+  while (1) {
+    now = NDL_GetTicks();
+    if (now - last >= 1000) {
+      last = now;
+      printf("timer-test:%d\n", i++);
+    }
+  }
   return 0;
 
 }
