f81718075e624a05fc08854a5449d5eadd9dc06f
tracer-ics2022 <tracer@njuics.org>
2023-08-04 15:48:44 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:48:44 up  3:12,  1 user,  load average: 4.34, 2.47, 1.31

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -16,7 +16,7 @@ static const char* keyname[256] __attribute__((used)) = {
 
 size_t serial_write(const void* buf, size_t offset, size_t len) {
 
-  yield();
+  // yield();
 
   for (size_t i = 0; i < len; i++) {
     printf("1");
