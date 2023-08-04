d4c3f6781cd7af9339d8b87a05eee1c24b8a0a4d
tracer-ics2022 <tracer@njuics.org>
2023-08-04 16:24:39 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:24:39 up  3:48,  1 user,  load average: 0.88, 0.84, 0.85

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,7 +25,7 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-  naive_uload(NULL, "/dev/events");
+  naive_uload(NULL, "/bin/hello");
 }
 
 Context* schedule(Context *prev) {
