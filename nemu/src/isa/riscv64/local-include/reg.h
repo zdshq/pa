7e71ab6d748c6dfa071272e488f1a38ee8fd2594
tracer-ics2022 <tracer@njuics.org>
2023-08-05 11:12:12 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:12:12 up 27 min,  1 user,  load average: 0.51, 0.78, 0.53

--- nemu/src/isa/riscv64/local-include/reg.h
+++ nemu/src/isa/riscv64/local-include/reg.h
@@ -35,7 +35,8 @@ static inline int check_csr_idx(int idx) {
 }
 
 #define csr(idx) (cpu.csr[check_csr_idx(idx)])
-
+#define csrmepc (cpu.csr[mepc])
+#define csrmstatus (cpu.csr[mstatus])
 #define gpr(idx) (cpu.gpr[check_reg_idx(idx)])
 
 static inline const char* reg_name(int idx, int width) {
