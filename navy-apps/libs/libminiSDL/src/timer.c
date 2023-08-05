868e4115be65ec2fa99fb919b422f4b9e1c8188a
tracer-ics2022 <tracer@njuics.org>
2023-08-05 11:22:31 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:22:31 up 37 min,  1 user,  load average: 0.96, 0.82, 0.67

--- navy-apps/libs/libminiSDL/src/timer.c
+++ navy-apps/libs/libminiSDL/src/timer.c
@@ -1,18 +1,28 @@
 #include <NDL.h>
 #include <sdl-timer.h>
 #include <stdio.h>
-
-SDL_TimerID SDL_AddTimer(uint32_t interval, SDL_NewTimerCallback callback, void *param) {
+#include <assert.h>
+SDL_TimerID SDL_AddTimer(uint32_t interval, SDL_NewTimerCallback callback, void* param) {
+  printf("SDL_AddTimer\n");
+  assert(0);
   return NULL;
 }
 
 int SDL_RemoveTimer(SDL_TimerID id) {
+  printf("SDL_RemoveTimer\n");
+  assert(0);
   return 1;
 }
 
 uint32_t SDL_GetTicks() {
-  return 0;
+  return  NDL_GetTicks();
 }
-
+// 速度过慢,不需要延迟
 void SDL_Delay(uint32_t ms) {
+  // printf("SDL_Delay\n");
+  // uint32_t now = SDL_GetTicks();
+  // while (SDL_GetTicks() - now < ms) {
+  //   //printf("SDL_Delay\n");
+  // }
+  //assert(0);
 }
