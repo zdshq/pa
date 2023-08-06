6f07a0e613143de4bf4ccbb08e12a32f3de8a13e
tracer-ics2022 <tracer@njuics.org>
2023-08-06 17:37:54 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:37:54 up  4:22,  1 user,  load average: 1.28, 0.98, 0.87

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,15 +32,13 @@ void init_proc() {
 }
 
 Context*  schedule(Context* prev) {
-  // save the context pointer
-  current->cp = prev;
+// save the context pointer
+current->cp = prev;
 
-  // always select pcb[0] as the new process
-  // current = &pcb[0];
-  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+// always select pcb[0] as the new process
+current = &pcb[0];
 
-  // then return the new context
-  printf("pcb[0].cp : %d\n", (int32_t)pcb[0].cp->mepc);
-  return pcb[0].cp;
+// then return the new context
+return current->cp;
 
 }
