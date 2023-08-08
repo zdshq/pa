af1ff2274f4ab280ee55988e3019fad0861b3d84
tracer-ics2022 <tracer@njuics.org>
2023-08-08 10:57:26 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:57:26 up  1:08,  1 user,  load average: 1.71, 1.28, 0.98

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -1,36 +1,43 @@
 #include <memory.h>
 
-static void* pf = NULL;
+static void *pf = NULL;
 
-void* new_page(size_t nr_page) {
+void *new_page(size_t nr_page)
+{
 
-  void* last_pf = pf;
+  void *last_pf = pf;
   uintptr_t pf_temp = (uintptr_t)pf;
-  pf = (void*)(pf_temp + nr_page * PGSIZE);
+  pf = (void *)(pf_temp + nr_page * PGSIZE);
 
   assert((uintptr_t)pf == (pf_temp + nr_page * PGSIZE));
   return last_pf;
 }
 
 #ifdef HAS_VME
-static void* pg_alloc(int n) {
+static void *pg_alloc(int n)
+{
   void *mem = new_page(n);
-  memset(mem, 0, n*PGSIZE);
+  Log("1?\n");
+  memset(mem, 0, n * PGSIZE);
+  Log("2?\n");
   return mem;
 }
 #endif
 
-void free_page(void* p) {
+void free_page(void *p)
+{
   panic("not implement yet");
 }
 
 /* The brk() system call handler. */
-int mm_brk(uintptr_t brk) {
+int mm_brk(uintptr_t brk)
+{
   return 0;
 }
 
-void init_mm() {
-  pf = (void*)ROUNDUP(heap.start, PGSIZE);
+void init_mm()
+{
+  pf = (void *)ROUNDUP(heap.start, PGSIZE);
   Log("free physical pages starting from %p", pf);
 
 #ifdef HAS_VME
