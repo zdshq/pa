804649aa1d2c6acbc202ec2ae7a11cf11f49005a
tracer-ics2022 <tracer@njuics.org>
2023-08-08 10:37:41 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:37:41 up 48 min,  1 user,  load average: 0.92, 0.94, 0.69

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -43,5 +43,23 @@ int isa_mmu_check(vaddr_t vaddr, int len, int type) {
 }
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
-  return MEM_RET_FAIL;
+    uint32_t *pdir = (uint32_t *)(cpu.csr[4] << 12); // 获得页表基地址
+    if(pdir == NULL){
+      return MMU_FAIL;
+    }
+    uint32_t *pte = pdir + ((uintptr_t)vaddr >> 22) * 4; // 获得一级页表的物理地址
+    if(pte == NULL){
+      return MMU_FAIL;
+    }
+    uint32_t *pde = (uint32_t *)((uintptr_t)(*pte) >> 12);
+    if(pde == NULL){
+      return MMU_FAIL;
+    }
+    if ((*pde >> 12) == ((uintptr_t)vaddr >> 12)) {
+        return MMU_DIRECT;
+    }
+    else{
+      return MMU_TRANSLATE;
+    }
+    return ((*pde) & (~0xfff)) + (vaddr & 0xfff);
 }
