6952e2860778d91d7d88fe584bf09c51f2fb4c46
tracer-ics2022 <tracer@njuics.org>
2023-08-10 16:49:34 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:49:34 up 33 min,  1 user,  load average: 1.36, 0.93, 0.75

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -131,9 +131,9 @@ extern uint8_t pmem[CONFIG_MSIZE];
 // }
 
 static int cmd_s(char *args){
-  char filename[100] = "/home/zsx/ics2022/nemu/quickshot/";
-  strcat(filename, args);
-  FILE *fd = fopen(filename, "w");
+  char filename[100] = "/home/zsx/ics2022/nemu/quickshot/1";
+  // strcat(filename, args);
+  FILE *fd = fopen(filename, "rw");
   assert(fd != NULL);
   printf("1122\r\n");
   fwrite(pmem, CONFIG_MSIZE/2, 1, fd);
