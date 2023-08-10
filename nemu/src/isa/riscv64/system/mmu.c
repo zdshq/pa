d51f10606c05f9c6edaf6583b903cc5a72843658
tracer-ics2022 <tracer@njuics.org>
2023-08-10 22:44:19 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:44:19 up 32 min,  1 user,  load average: 0.39, 0.37, 0.34

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -17,29 +17,35 @@
 #include <memory/vaddr.h>
 #include <memory/paddr.h>
 
+
 /**
  * @param type : 读写类型，目前根据kiss原则，先不做区分
 */
 int isa_mmu_check(vaddr_t vaddr, int len, int type) {
-    uint32_t *pdir = (uint32_t *)(cpu.csr[4] << 12); // 获得页表基地址
-    // paddr_read(pdir)
+    uint32_t pdir = (uint32_t *)(cpu.csr[4] << 12); // 获得页表基地址
     if(pdir == NULL){
       return MMU_FAIL;
     }
-    uint32_t *pte = pdir + ((uintptr_t)vaddr >> 22) * 4; // 获得一级页表的物理地址
-    if(pte == NULL){
-      return MMU_FAIL;
-    }
-    uint32_t *pde = (uint32_t *)((uintptr_t)(*pte) >> 12);
-    if(pde == NULL){
-      return MMU_FAIL;
-    }
-    if ((*pde >> 12) == ((uintptr_t)vaddr >> 12)) {
-        return MMU_DIRECT;
-    }
-    else{
-      return MMU_TRANSLATE;
-    }
+    uint32_t pde_index = vaddr >> 22;
+    uint32_t pte_index = vaddr >> 12 & 0x3ff;
+    uint32_t pde = pdir + ((uintptr_t)vaddr >> 22) * 4; // 获得一级页表的物理地址
+    pde = paddr_read(pde, 4);
+    uint32_t pte = pde >> 12 + 4 * pte_index;
+    printf("pde : %x\n", pde);
+    // if((((pte) >> 2))  == NULL){
+    //   return MMU_FAIL;
+    // }
+    // uint32_t *pde = (uint32_t *)((uintptr_t)(*pte) >> 12);
+    // if(pde == NULL){
+    //   return MMU_FAIL;
+    // }
+    // if ((*pde >> 12) == ((uintptr_t)vaddr >> 12)) {
+    //     return MMU_DIRECT;
+    // }
+    // else{
+    //   return MMU_TRANSLATE;
+    // }
+    return 0;
 }
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
