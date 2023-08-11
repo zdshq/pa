2e179b59a0675a9c5cc1afcd207cc795c659f68d
tracer-ics2022 <tracer@njuics.org>
2023-08-11 16:58:33 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:58:33 up 58 min,  1 user,  load average: 0.36, 0.34, 0.44

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -93,7 +93,7 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
     pde->write = (prot >> 1) & 1;
     pde->phy = (uintptr_t)as->ptr + ((pde_idx << 10) * 4 >> 12);
     printf("pde : %p\t pte : %p\n", pde, pte);
-    printf("%p\n", *(uint32_t *)(pte));
+    printf("%p\n", pte->val);
     printf("%p\n", pte->phy);
   }
 }
