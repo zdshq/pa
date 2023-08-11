3844fb649ed89372c99b9519b607ffc54a76bf4d
tracer-ics2022 <tracer@njuics.org>
2023-08-11 20:55:48 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:55:48 up  2:51,  1 user,  load average: 0.65, 0.42, 0.52

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -33,9 +33,9 @@ int isa_mmu_check(vaddr_t vaddr, int len, int type) {
     uint32_t pte_index = vaddr >> 12 & 0x3ff;
     uint32_t pde = paddr_read(pdir + pde_index * 4, 4); // 获得一级页表的物理地址
     uint32_t pte = paddr_read(pdir + (pde_index << 10) * 4 + pte_index * 4, 4);
-    if(!(pde & (1 << 2)) && !(pte & (1 << 2)))
+    if(!(pde & (1 << 2)) || !(pte & (1 << 2)))
     {
-      // printf("22\n");
+      printf("22\n");
       return MMU_FAIL;
     }
     // if()
