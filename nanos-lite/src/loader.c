7600b6393fb589210d4877af9c08d41d174efa8b
tracer-ics2022 <tracer@njuics.org>
2023-08-12 14:57:11 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:57:11 up 22 min,  1 user,  load average: 0.58, 0.72, 0.81

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -141,7 +141,7 @@ void context_uload(PCB* pcb_p, const char* filename, char* const argv[], char* c
   protect(&(pcb_p->as));
   Log("11\n");
   printf("pcb->as:%p\n", &(pcb_p->as));
-  asm volatile("csrw satp, %0" : : "r"(((uintptr_t)&(pcb_p->as) >> 12)));
+  asm volatile("csrw satp, %0" : : "r"(((uintptr_t)&(pcb_p->as))));
   Log("12\n");
   char* ustack_start = (char*)new_page(8);
   Log("13\n");
