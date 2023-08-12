cd14786884f0a22b76dc57173a788c738d399806
tracer-ics2022 <tracer@njuics.org>
2023-08-12 14:57:53 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:57:53 up 23 min,  1 user,  load average: 1.20, 0.89, 0.87

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -15,7 +15,7 @@ static Area segments[] = {      // Kernel memory mappings
 #define USER_SPACE RANGE(0x40000000, 0x80000000)
 
 static inline void set_satp(void *pdir) {
-  asm volatile("csrw satp, %0" : : "r"(((uintptr_t)pdir >> 12)));
+  asm volatile("csrw satp, %0" : : "r"(((uintptr_t)pdir)));
 }
 
 static inline uintptr_t get_satp() {
