bf6feb8b37b5efc20e3e3bd827f2ef17e5d29782
tracer-ics2022 <tracer@njuics.org>
2023-08-13 16:42:44 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:42:44 up  1:04,  1 user,  load average: 0.71, 0.73, 0.74

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -30,9 +30,11 @@ Context* __am_irq_handle(Context* c) {
     //   printf("%d: %x\n", i, c->gpr[i]);
     // }
     c = user_handler(ev, c);
-    // printf("hhh: %d", c->mepc);
+    printf("hhh: 111111\n");
     assert(c != NULL);
+    printf("hhh: 222222\n");
     __am_switch(c);
+    printf("hhh: 333333\n");
   }
 
   return c;
