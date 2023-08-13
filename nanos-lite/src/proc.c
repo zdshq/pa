5b729065d35831431557017e3838106c5a98a8b4
tracer-ics2022 <tracer@njuics.org>
2023-08-13 19:48:15 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:48:15 up  4:10,  1 user,  load average: 0.74, 0.81, 0.51

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,8 +32,8 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  context_kload(&pcb[1], hello_fun, NULL);
-  context_uload(&pcb[0], "/bin/menu", pal_argv, pal_envp);
+  context_kload(&pcb[0], hello_fun, NULL);
+  context_uload(&pcb[1], "/bin/menu", pal_argv, pal_envp);
   Log("wuyu");
   // context_kload(&pcb[0], hello_fun, NULL);
   // printf("pcb[0].cp : %d\n", (int32_t)pcb[0].cp->mepc);
