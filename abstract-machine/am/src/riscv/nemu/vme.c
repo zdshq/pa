4da40e93b18549a50e6f4c7730155da0b76944df
tracer-ics2022 <tracer@njuics.org>
2023-08-13 16:14:05 +0800

    >  compile NEMU 2020112315 张顺心 Linux zsx 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:14:05 up 36 min,  1 user,  load average: 6.85, 2.90, 1.48

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -62,13 +62,13 @@ void protect(AddrSpace *as) {
   if(m == 2){
       printf("hahahaerwer\t\n");
   }
-  int i = 0;
+  // int i = 0;
   void *va = as->area.start;
   for (; va < as->area.start + (as->area.end - as->area.start) / 8; va += PGSIZE) {
-    if(m == 2){
-      i++;
-      printf("hahahaerwer\t i : %d\n", i);
-    }
+    // if(m == 2){
+    //   i++;
+    //   printf("hahahaerwer\t i : %d\n", i);
+    // }
     void *pa = pgalloc_usr(1);
     map(as, va, pa, 0);
   }
